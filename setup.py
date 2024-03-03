d2afa3a3f9272fa9a7a9deb67ccf1a151117161a
rushikeshkusare <rushikeshkkusare29@gmail.com>
2024-03-04 02:24:19 +0530

    packaging setup and config

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 from setuptools import setup, find_packages
 from typing import List
+from pathlib import Path
 
+HYPEN_E_DOT = '-e .'
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
-   
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+__version__ = "0.0.1"
+REPO_NAME = "Python_Pakaging"
+PKG_NAME= "db_connect"
+AUTHOR_USER_NAME = "rushikeshkusare"
+AUTHOR_EMAIL = "rushikeshkkusare29@gmail.com"
 
 setup(
     name=PKG_NAME,
@@ -25,4 +38,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires = get_requirements(Path("requirements_dev.txt"))
     )
